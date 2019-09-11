fa583a416252467fd4fe14d494d7512977b6869a
RyanVries <45096137+RyanVries@users.noreply.github.com>
2019-09-11 10:54:38 +0200

    Meer

--- practicals/week_1_knn.py
+++ practicals/week_1_knn.py
@@ -1,14 +1,7 @@
 import numpy as np
-from sklearn.datasets import load_breast_cancer
+from sklearn.datasets import load_breast_cancer, load_diabetes
 import matplotlib.pyplot as plt
 import operator
-
-breast_cancer = load_breast_cancer()
-
-X_train = breast_cancer.data[:300, :]
-y_train = breast_cancer.target[:300, np.newaxis]
-X_test = breast_cancer.data[300:, :]
-y_test = breast_cancer.target[300:, np.newaxis]
 
 def euclidean_distance(vector1, vector2):
     return np.linalg.norm(vector1-vector2)
@@ -30,13 +23,16 @@ def get_neighbours(X_train, y_train, testInstance, k):
     Returns:
         [list] -- [of length(k). Contains the data for the k closest 
         neighbours of testInstance. ]
+        [list] -- of length k. Contains the distances of the closest k 
+        neighbours of TestInstance.
     """
     trainData = np.concatenate((X_train, y_train), axis=1)          #creates combined matrix of data and label
 
+    
     distances = []
     for i in range(len(trainData)):                                 #calculate distance from testInstance to every trainDatapoint
-        dist = euclidean_distance(testInstance, trainData[i][:30])
-        distances.append((trainData[i], dist))
+        dist = euclidean_distance(testInstance, trainData[i][:np.shape(X_train)[1]])
+        distances.append((trainData[i,:], dist))
     distances.sort(key=operator.itemgetter(1))
     neighbours = []
     for x in range(k):                                              #get the k-nearest
@@ -63,7 +59,8 @@ def get_response(neighbours):
     sortedVotes = sorted(classvotes.items(), key=operator.itemgetter(1), reverse=True)
     return sortedVotes[0][0]
 
-def perform_knn(X_train, y_train, X_test, k):
+
+def perform_knn(X_train, y_train, X_test, k, regression):
     """Creates testData, which is a matrix of X_test, with the predicted labels based
     on k nearest neighbours. 
     
@@ -74,19 +71,61 @@ def perform_knn(X_train, y_train, X_test, k):
         k {[integer]} -- [number of neighbours to take into account]
     
     Returns:
-        [matrix] -- [X_test with the predicted labels]
+        [matrix] -- [X_test with the predicted labels or predicted values 
+        dependent on classification or regression]
     """
-    X_train = normalize(X_train)
-    X_test = normalize(X_test)
+    if regression!=True:
+        X_train = normalize(X_train)
+        X_test = normalize(X_test)
+    elif regression:
+        prediction=np.zeros((np.shape(X_test)[0],np.shape(X_test)[1]))
 
     testData = np.concatenate((X_test, np.zeros((len(X_test),1))), axis=1)  #initialize testData with empty labels, to replace later with predicted labels. 
 
     for i in range(len(X_test)):
         neighbours = get_neighbours(X_train, y_train, X_test[i], k)
-        predicted_label = get_response(neighbours)
-        testData[i][-1] = predicted_label                                   #replace the zerolabel in testData with the predicted label
+        if regression:
+            predict=np.zeros((np.shape(X_train)[1]))
+            for neig in neighbours:
+                predict += neig[:np.shape(X_train)[1]]
+            prediction[i,:]=predict/k
+        else:
+            predicted_label = get_response(neighbours)
+            testData[i][-1] = predicted_label    
+                               #replace the zerolabel in testData with the predicted label
+    if regression:
+        return prediction
+    else:
+        return testData
 
-    return testData
+def mse(truth,predict):
+    '''Calculate the mean squared errors when cosidering multiple dimensions'''
+    return np.mean(np.mean(np.square(np.subtract(truth,predict)),axis=1))
+
+def regression_plot():
+    '''Create a plot to show the MSE of the kNN regression algorithm for 
+    different k values'''
+    
+    #download the necessary data
+    diabetes = load_diabetes()
+
+    X_train = diabetes.data[:300, :]
+    y_train = diabetes.target[:300, np.newaxis]
+    X_test = diabetes.data[300:, :]
+    
+
+    a=25
+    errors=np.zeros(a)
+    for idx, k in enumerate(range(1,2*a,2)):
+        prediction=perform_knn(X_train, y_train, X_test, k, True)
+        errors[idx]=mse(X_test,prediction)
+    plt.plot(range(1,2*a,2),errors)
+    plt.xlabel('k (Number of considered neighbours)')
+    plt.ylabel('Mean Squared Error (-)')
+    plt.title('MSE for k Nearest Neighbour regression')
+    plt.show()
+    
+    return
 
 def get_accuracy(testData, y_test):
     prediction = testData[:,-1]
@@ -94,4 +133,12 @@ def get_accuracy(testData, y_test):
     return correct    
 
 def plot_error(testData, y_test, k):
-    #TODO    
+    breast_cancer = load_breast_cancer()
+
+    X_train = breast_cancer.data[:300, :]
+    y_train = breast_cancer.target[:300, np.newaxis]
+    X_test = breast_cancer.data[300:, :]
+    y_test = breast_cancer.target[300:, np.newaxis] 
+    return
+
+
