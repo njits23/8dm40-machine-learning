50fd886735b37a55867bea819a9b12a8f95e0b82
Vries <r.d.vries.2@student.tue.nl>
2019-10-12 23:09:58 +0200

    merge

--- practicals/code week 4/func_wk4.py
+++ practicals/code week 4/func_wk4.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 def brightness_aug(train_images, train_segmentations):
-    mult=7;   #number of augmented images generated from each original image 
+    mult=6;   #number of augmented images generated from each original image 
     sh=np.shape(train_images);
     images=np.zeros((sh[0]*mult,sh[1],sh[2],sh[3]))
     segmentations=np.zeros((sh[0]*mult,sh[1],sh[2],1))
-    ba_range=np.linspace(0,1.2,mult)   #different multipliers for the images
+    ba_range=np.linspace(0.2,1.2,mult)   #different multipliers for the images
     
     for idx, image in enumerate(train_images):
         segmentations[idx*mult:(idx+1)*mult]=train_segmentations[idx]
